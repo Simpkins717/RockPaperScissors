3494f0fe1338c118f7a922a38eb0c5df7bf40851
Simpkins717 <dsimpkins717@gmail.com>
2022-06-14 11:29:34 -0400

    RPS ui quick

--- RPS.js
+++ RPS.js
@@ -1,38 +1,41 @@
-let computerPlay = function () {
-  let choice = Math.floor(Math.random() * 12) + 1;
-  if (choice <= 4) {
-    return "Rock";
-  } else if (choice < 8 && choice > 4) {
-    return "Paper";
-  } else {
-    return "Scissors";
-  }
-};
-let computerSelection = computerPlay().toUpperCase();
-let playerSelection = prompt(
-  "What is your Rock,Paper,Scissors Selection?"
-).toUpperCase();
+const myArray = ["Rock", "Paper", "Scissors"];
 
-let playRound = function (playerSelection, computerSelection) {
-  if (computerSelection === "ROCK" && playerSelection === "PAPER") {
-    console.log(`You Win! ${playerSelection} beats            
-      ${computerSelection}}`);
-  } else if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
-    console.log(`You Lose ${computerSelection} beats            
-      ${playerSelection}}`);
-  } else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
-    console.log(`You Lose! ${computerSelection} beats            
-      ${playerSelection}}`);
-  } else if (computerSelection === "PAPER" && playerSelection === "SCISSORS") {
-    console.log(`You Win! ${playerSelection} beats            
-      ${computerSelection}}`);
-  } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK") {
-    console.log(`You Win! ${playerSelection} beats            
-      ${computerSelection}}`);
-  } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
-    console.log(`You Lose! ${computerSelection} beats            
-      ${playerSelection}}`);
-  } else {
-    console.log(`You tie! ${playerSelection} equals ${computerSelection}`);
+function computerPlay() {
+  return myArray[~~(Math.random() * myArray.length)];
+}
+let playerScore = 0;
+let computerScore = 0;
+let computerSelection;
+let playerSelection;
+function playRound(playerSelection, computerSelection) {
+  computerSelection = computerPlay().toLowerCase();
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection === computerSelection) {
+    winner = "tie";
   }
-};
+  if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK")
+  ) {
+    playerScore++;
+  }
+  if (
+    (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+    (computerSelection === "SCISSORS" && playerSelection === "PAPER") ||
+    (computerSelection === "PAPER" && playerSelection === "ROCK")
+  ) {
+    computerScore++;
+  }
+}
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
+rockBtn.addEventListener("click", () => clickHandler("Rock"));
+paperBtn.addEventListener("click", () => clickHandler("Paper"));
+scissorsBtn.addEventListener("click", () => clickHandler("Scissors"));
+
+function clickHandler(playerSelection) {
+  const computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+}
